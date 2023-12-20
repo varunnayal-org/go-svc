ae69a9a7fbf12cb934f2a3e7bf33e9af053c6252
Varun <varunnayal@gmail.com>
2023-12-20 17:36:57 +0530

    feature jira and lint

--- migrations/20231017080505.sql
+++
@@ -1,9 +0,0 @@
--- Create "cities" table
-CREATE TABLE "public"."cities" (
-  "id" text NOT NULL,
-  "created_at" timestamptz NOT NULL,
-  "updated_at" timestamptz NOT NULL,
-  "is_active" boolean NULL DEFAULT true,
-  "name" character varying(100) NOT NULL,
-  PRIMARY KEY ("id")
-);
--- migrations/20231120142300.sql
+++
@@ -1,5 +0,0 @@
--- Create "friends" table
-CREATE TABLE "public"."friends" (
-  "user_id" text NOT NULL,
-  "friend_id" text NOT NULL
-);
